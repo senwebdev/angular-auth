9c3e902baa7dae5dfb163fe20d86741a12abef2e
strahinjarepo <dev.rsportsbaseball@gmail.com>
2019-01-30 20:50:47 +0800

    Angular Authentication

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
+import { moveIn } from '../router.animations';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  animations: [moveIn()],
+  host: {'[@moveIn]': ''}
+})
+export class LoginComponent implements OnInit {
+
+  error: any;
+  constructor(public af: AngularFireAuth, private router: Router){  }
+
+  loginFb(){
+    this.af.auth.login({
+      provider: AuthProviders.Facebook,
+      method: AuthMethods.Popup,
+    }).then(
+      (success)=>{
+        this.router.navigate(['/members']);
+      }
+    ).catch(
+      (err)=>{
+        this.error = err;
+      }
+    )
+  }
+
+  loginGoogle(){
+    this.af.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup,
+    }).then(
+      (success)=>{
+        this.router.navigate(['/members']);
+      }
+    ).catch(
+      (err)=>{
+        this.error = err;
+      }
+    )
+  }
+
+  ngOnInit() {
+  }
+
+}
